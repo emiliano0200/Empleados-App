9bc66ae55ff24488d3bb9559d6ec959cb9e35ac2
emiliano0200 <empleadoseea@gmail.com>
2023-11-30 21:36:39 -0600

    Inicio

---
+++ src/app/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDj9-Gm9hvNr4Jqm1q7Drhw3aCEwQe-_ec",
+    authDomain: "paises-app-edu.firebaseapp.com",
+    projectId: "paises-app-edu",
+    storageBucket: "paises-app-edu.appspot.com",
+    messagingSenderId: "7453355958",
+    appId: "1:7453355958:web:4dbb75e725cc651b0b1b17"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
